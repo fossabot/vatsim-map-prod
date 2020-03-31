fa1d1bba410b3e14489054c77305ff703ec949d6
Mark Doyle <mark@markdoyle.dev>
2020-03-31 02:36:43 +0100

    Add error logging to prod

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 // plugins
 Vue.use(VueCompositionApi);
-
 Vue.config.productionTip = false;
+
+// enable error logging
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://dadece6a2a2c4518b794862a500456b2@sentry.io/5182235',
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+  });
+}
 
 new Vue({
   router,
